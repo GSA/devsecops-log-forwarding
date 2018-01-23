4a522e067a1addd4cc2a0fc1f6c3392ea639ec77
Aidan Feldman <aidan.feldman@gsa.gov>
2018-01-23 11:11:01 -0500

    inital copy of logging code (broken)

---
+++ test/main.tf
@@ -0,0 +1,35 @@
+module "network" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = ">= 1.11.0"
+
+  azs = ["${local.azs}"]
+  cidr = "${var.vpc_cidr}"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  enable_nat_gateway = true
+  name = "devsecops-example-mgmt"
+  public_subnets = ["${var.public_subnet_cidr}"]
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["*ubuntu-xenial-16.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+module "log_forwarding" {
+  source = ".."
+}
