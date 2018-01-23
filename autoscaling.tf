c7e43004c7f0b844e277d84d93870e4a2803197c
Aidan Feldman <aidan.feldman@gsa.gov>
2018-01-23 11:30:22 -0500

    split load balancing and autoscaling to separate files

--- autoscaling.tf
+++ autoscaling.tf
@@ -28,29 +28,3 @@ resource "aws_autoscaling_group" "log_forwarding" {
     propagate_at_launch = true
   }
 }
-
-resource "aws_alb" "log_forwarding" {
-  # TODO make internal?
-  # internal = true
-  # https://aws.amazon.com/elasticloadbalancing/details/#compare
-  load_balancer_type = "network"
-  # TODO change to private
-  subnets = ["${var.public_subnets}"]
-}
-
-resource "aws_alb_target_group" "log_forwarding" {
-  vpc_id = "${var.vpc_id}"
-  port = "${local.logging_port}"
-  protocol = "${local.logging_protocol}"
-}
-
-resource "aws_alb_listener" "log_forwarding" {
-  load_balancer_arn = "${aws_alb.log_forwarding.arn}"
-  port = "${local.logging_port}"
-  protocol = "${local.logging_protocol}"
-
-  default_action {
-    target_group_arn = "${aws_alb_target_group.log_forwarding.arn}"
-    type = "forward"
-  }
-}
